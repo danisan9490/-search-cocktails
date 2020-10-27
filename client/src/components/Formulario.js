aba356dd4b394e0b0bf327a5d63fa024c863826b
Daniel <danisan9490@gmail.com>
2020-10-27 15:49:22 +0000

    #2 Add Form

--- client/src/components/Formulario.js
+++ client/src/components/Formulario.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React from 'react'
 
 const Formulario = () => {
   return (
-    <h1>Form</h1>
+    <form
+      className="col-12"
+    >
+      <fieldset className="text-center">
+        <legend>Search by Ingredient or Category</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="nombre"
+            className="form-control"
+            type="text"
+            placeholder="Search by Ingredient"
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="categoria"
+          >
+            <option value="">-- Select Category --</option>
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
   );
 }
 
